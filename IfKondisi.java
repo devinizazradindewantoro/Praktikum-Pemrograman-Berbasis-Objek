1f9f4ff067089d3d57793165f15749d6ab6aa326
devinizazradindewantoro <darkdevin469@gmail.com>
2023-09-29 14:30:08 +0700

    Jobsheet5

--- IfKondisi.java
+++ IfKondisi.java
@@ -3,7 +3,7 @@ public class IfKondisi {
     public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
 
-    System.out.print("Masukkan pilihan jarak : ");
+    System.out.print("Masukkan jarak (dalam m) : ");
     Integer mode = input.nextInt();
 
     if (mode<=5) {
